a3058e5f56ff720375e49f74a5b56561ce03f261
Amanda Malamis <sitkoama@gmail.com>
2023-09-11 16:25:32 -0500

    authentication, middleware

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,28 @@
 const express = require('express')
-const dotenv = require('dotenv').config()
+const colors = require('colors')
+
+const dotenv = require('dotenv')
+dotenv.config()
+const { errorHandler } = require('./middleware/errorMiddleware')
+const connectDB = require('./config/db')
+
 const PORT = process.env.PORT || 5000
 
+//connect to DB
+connectDB()
+
 const app = express()
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+
 app.get('/', (req, res) => {
-    res.send("Hello")
+    res.status(200).json({ message: "Support Desk API" })
 })
 
+//Routes
+app.use('/api/users', require('./routes/userRoutes'))
 
+app.use(errorHandler)
 
-app.listen(PORT, () => console.log('Server started'))
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
