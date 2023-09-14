a98cb3df7224ded7e4bda5542b83d860eb098052
Amanda Malamis <sitkoama@gmail.com>
2023-09-14 12:34:46 -0500

    updates to routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,9 @@ import Header from './components/Header';
 import Home from './pages/Home';
 import Login from './pages/Login';
 import Register from './pages/Register';
+import NewTicket from './pages/NewTicket';
+import PrivateRoute from './components/PrivateRoute';
+
 
 function App() {
   return (
@@ -16,7 +19,10 @@ function App() {
             <Route path='/' element={<Home></Home>} ></Route>
             <Route path='/login' element={<Login></Login>} ></Route>
             <Route path='/register' element={<Register></Register>} ></Route>
-
+            {/* //to create a  private route, wrap it in this way */}
+            <Route path='/new-ticket' element={<PrivateRoute></PrivateRoute>} >
+              <Route path='/new-ticket' element={<NewTicket></NewTicket>} ></Route>
+            </Route>
           </Routes>
         </div>
       </Router>
